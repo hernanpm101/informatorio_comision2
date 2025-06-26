46d6a75d71208d372af386dd49bda20df8fc159c
hernanpm101 <hernanpm101@hotmail.com>
2025-06-26 09:23:54 -0300

    RelojApp en Tkinder/Python

--- reloj_v2.py
+++ reloj_v2.py
@@ -18,7 +18,7 @@ class RelojApp(tk.Tk):
             "November": "Noviembre", "December": "Diciembre"
         }
         
-        self.title("Alumnos: Roma, Delgado Labrovich y Perez Melgar. Grupo 12-TKinder miniApp")
+        self.title("Alumnos: Roma, Delgado Labrovich y Perez Melgar. Grupo 12-TKinder RelojApp")
         self.geometry("600x400") 
         self.resizable(False, False)
         self.configure(bg="black")
